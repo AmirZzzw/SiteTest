a7a788cfbbc3397577cda916407ab7294cad4752
Amir <amirmohammadyosefi290@gmail.com>
2025-12-05 20:11:42 +0330

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 // supabase-config.js
 export const SUPABASE_CONFIG = {
     URL: 'https://oudwditrdwugozxizehm.supabase.co',
-    ANON_KEY: 'sb_publishable_K-eXHsnknpw5im47hnI-Tw_kwtT_V5S'
+    ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91ZHdkaXRyZHd1Z296eGl6ZWhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4ODQzMTcsImV4cCI6MjA4MDQ2MDMxN30.BQxoJD-WnRQQvIaQQSTzKzXLxf2LdGuPkqBCKvDruGE'
 };
