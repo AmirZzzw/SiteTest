0235e383de63023e94265b2ab02062b2d959f219
Amir <amirmohammadyosefi290@gmail.com>
2025-12-08 04:39:51 +0330

    Update supabase-service.js

--- supabase-service.js
+++ supabase-service.js
@@ -4,7 +4,7 @@ console.log('📦 Loading Supabase service...');
 // تنظیمات Supabase
 const SUPABASE_CONFIG = {
     URL: 'https://oudwditrdwugozxizehm.supabase.co',
-    ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91ZHdkaXRyZHd1Z296eGl6ZWhtIiwicm9sZURI6ImFub24iLCJpYXQiOjE3NjQ4ODQzMTcsImV4cCI6MjA4MDQ2MDMxN30.BQxoJD-WnRQQvIaQQSTzKzXLxf2LdGuPkqBCKvDruGE'
+    ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91ZHdkaXRyZHd1Z296eGl6ZWhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4ODQzMTcsImV4cCI6MjA4MDQ2MDMxN30.BQxoJD-WnRQQvIaQQSTzKzXLxf2LdGuPkqBCKvDruGE'
 };
 
 // ایجاد کلاینت Supabase
